ffa70ea614f2da0278ede32feb55da15dcc0bf2a
N <Jagadish.N2@tui.co.uk>
2018-06-29 17:02:18 +0100

    genereating json data on template match

--- image/bruteForeMatch.py
+++ image/bruteForeMatch.py
@@ -6,7 +6,8 @@ import templateMatch
 def bruteForeMatch(snapshot,features):
 	for x in features:
 		img1 = cv.imread('./image.jpg',0)          # queryImage
-		img2 = cv.imread(x,0) # trainImage
+		img2 = cv.imread(x['feature-image'],0) # trainImage
+	
 		# Initiate ORB detector
 		orb = cv.ORB_create()
 		# find the keypoints and descriptors with ORB
@@ -36,15 +37,14 @@ def bruteForeMatch(snapshot,features):
 			# y - rows
 			# Get the coordinates
 			(x1,y1) = kp1[img1_idx].pt
-			(x2,y2) = kp2[img2_idx].pt
+			(x2,y2) = kp2[img2_idx].pt 
 			
-			cv2.rectangle(image, min_loc, max_loc, 0, 2)
-	
-			plt.subplot(121),plt.imshow(image,cmap = 'gray')
-			plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-			plt.show()
-
-			# Append to each list
+			print(kp1[img1_idx])
+			print(kp2[img2_idx]) 
+			
+			
+			
+						# Append to each list
 			list_kp1.append((x1, y1))
 			list_kp2.append((x2, y2))
 			
@@ -52,15 +52,10 @@ def bruteForeMatch(snapshot,features):
 		print( list_kp2)
 	return matches
 
+features = [{'feature':'price-panel', 'feature-image':"./template.png"},
+			{'feature':'summary-panel','feature-image':"./template2.png"},
+            {'feature': 'gallery' , 'feature-image': "./template6.png"}]
 
-features = ["./f1.jpg", "./f2.jpg", "./template6.png"]	
-##matches=bruteForeMatch('./desk.jpg',features)
-b = templateMatch.templatematch('./desk.jpg',features)
-image = cv.imread('./desk.jpg',0)  
-for aa in b :
-  
-	cv.circle(image,aa[2], 10, (0,0,0))
-	plt.subplot(121),plt.imshow(image,cmap = 'gray')
-plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-plt.show()
+bruteForeMatch('./template', features)
+
 
--- image/templateMatch.py
+++ image/templateMatch.py
@@ -1,18 +1,49 @@
 import numpy as np
 import cv2 
 import matplotlib.pyplot as plt
+import json
+import math
 
 def templatematch(snapshot,features):
-	a=[]
-	for feature in features : 
-		image = cv2.imread(snapshot)
-		template = cv2.imread(feature)
-		res = cv2.matchTemplate(image,template,cv2.TM_CCORR_NORMED)
+	requiredJson=[]
+	page={'page':'flight_options'}
+	featureMatches={'count':2}
+	featuresdata=[]
+	for x in features:
+		img_rgb = cv2.imread(snapshot)
+		img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+		template = cv2.imread(x['feature-image'],0)
+		w, h = template.shape[::-1]
+
+		res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
 		min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res);
-		a.append([min_val, max_val, min_loc, max_loc,res])
-	return a	
+		threshold = 0.8
+		loc = np.where( res >= threshold)
+		if max_val >= threshold:
+			data = {}
+			data['name'] = x['feature']
+			
+			data['match']=max_val
+			featuresdata.append(data)
+			
+		
+		for pt in zip(*loc[::-1]):
+			cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+			cv2.imwrite('res.png',img_rgb)
+			
+			
+	featureMatches['features']=featuresdata
+	page['featureMatches']=featureMatches
+	requiredJson.append(page)
+	json_data = json.dumps(requiredJson)
+	f = open('./file.txt','w')
+	f.write(json_data)
+	f.close()
 
 
-	
+features1 = [{'feature':'price-panel', 'feature-image':"./f1.jpg"},
+			{'feature':'summary-panel','feature-image':"./f2.jpg"},
+            {'feature': 'gallery' , 'feature-image': "./template6.png"}]
+templatematch('desk.jpg',features1)	
 
 
